f67dc8aee371cf7f4a279ea0a2de333588225772
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2025-02-14 21:01:53 +0700

    Add item api routes

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Api\V1\ItemController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')
+    ->name('api.v1.')
+    ->group(function () {
+        Route::get('items', [ItemController::class, 'index'])->name('items.index');
+        Route::get('items/aggregations', [ItemController::class, 'aggregations'])->name(
+            'items.aggregations'
+        );
+        Route::get('items/suggestions', [ItemController::class, 'suggestions'])->name(
+            'items.suggestions'
+        );
+        Route::get('items/{id}', [ItemController::class, 'show'])->name('items.show');
+        Route::get('items/{id}/similar', [ItemController::class, 'similar'])->name(
+            'items.similar'
+        );
+        Route::post('items/{id}/views', [ItemController::class, 'incrementViewCount'])->name(
+            'items.views'
+        );
+    });
