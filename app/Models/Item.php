aef99fddf7dc2d8857f99b5a85b570b84d7b0599
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2025-02-14 20:55:03 +0700

    Add item factory

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,12 +7,13 @@ use App\Repositories\ItemRepository;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Item extends Model implements IndexableModel, TranslatableContract
 {
-    use Translatable;
+    use HasFactory, Translatable;
 
     protected $keyType = 'string';
 
